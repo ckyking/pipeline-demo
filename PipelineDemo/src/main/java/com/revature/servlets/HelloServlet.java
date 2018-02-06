7e00719de8da150c7212e0e4c8c18f4c4201d622
King Cheng <ckykingkk@gmail.com>
2018-02-06 14:49:38 -0500

    testing jenkins

---
+++ PipelineDemo/src/main/java/com/revature/servlets/HelloServlet.java
@@ -0,0 +1,16 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = -2347170500733541332L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		super.doGet(req, resp);
+	}
+}
